f032390765b3216903ae2db187fdb0ac9016187d
nguyen thanh giang <Anhgiang9831@gmail.com>
2021-02-26 15:49:36 +0700

    update menu admin

--- admin/menu.php
+++ admin/menu.php
@@ -9,26 +9,26 @@
 			<form action="" id="manage-menu">
 				<div class="card">
 					<div class="card-header">
-						    Menu Form
+						    Thôn tin thực đơn
 				  	</div>
 					<div class="card-body">
 							<input type="hidden" name="id">
 							<div class="form-group">
-								<label class="control-label">Menu Name</label>
+								<label class="control-label">Tên</label>
 								<input type="text" class="form-control" name="name">
 							</div>
 							<div class="form-group">
-								<label class="control-label">Menu Description</label>
+								<label class="control-label">Giới thiệu</label>
 								<textarea cols="30" rows="3" class="form-control" name="description"></textarea>
 							</div>
 							<div class="form-group">
 								<div class="custom-control custom-switch">
 								  <input type="checkbox" name="status" class="custom-control-input" id="availability" checked>
-								  <label class="custom-control-label" for="availability">Available</label>
+								  <label class="custom-control-label" for="availability">Có sẵn</label>
 								</div>
 							</div>	
 							<div class="form-group">
-								<label class="control-label">Category </label>
+								<label class="control-label">Loại thực đơn </label>
 								<select name="category_id" id="" class="custom-select browser-default">
 									<?php
 									$cat = $conn->query("SELECT * FROM category_list order by name asc ");
@@ -40,11 +40,11 @@
 								
 							</div>
 							<div class="form-group">
-								<label class="control-label">Price</label>
-								<input type="number" class="form-control text-right" name="price" step="any">
+								<label class="control-label">Giá</label>
+								<input type="number" placeholder="VNĐ" class="form-control text-right" name="price" step="any">
 							</div>
 							<div class="form-group">
-								<label for="" class="control-label">Image</label>
+								<label for="" class="control-label">Hình ảnh</label>
 								<input type="file" class="form-control" name="img" onchange="displayImg(this,$(this))">
 							</div>
 							<div class="form-group">
@@ -55,8 +55,8 @@
 					<div class="card-footer">
 						<div class="row">
 							<div class="col-md-12">
-								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Save</button>
-								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="$('#manage-menu').get(0).reset()"> Cancel</button>
+								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Lưu</button>
+								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="$('#manage-menu').get(0).reset()"> Hủy</button>
 							</div>
 						</div>
 					</div>
